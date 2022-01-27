0e541a3557d90840fc56f1176540b90ce1cc968c
DianaCostin02 <costin.diana02@gmail.com>
2022-01-27 08:38:01 +0100

    progetto_chatdasBallo

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+var nClien=-1;//perche se no conta ancheil terminale da cui controllo alias il server
+/*
+app.get('/', (req, res) => {
+  res.send('<h1>sciao bello</h1>');
+});*/
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  io.on('connection', (socket) => {
+    console.log('a user connected');
+    nClien++;;
+    console.log('user connessi: '+nClien);
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+      nClien--;
+      console.log('user connessi: '+nClien);
+    });
+  });
+  /*
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => { //se si vuole stalkerare i messggi
+      console.log('message: ' + msg);
+    });
+  });*/
+
+  io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+server.listen(3000, () => {
+  console.log('porta:3000 in ascolto');
+});
